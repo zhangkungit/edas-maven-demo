444f69ec280538c04a92fe8383c6aa3a48f3327e
zhangkun <zhangkun@yryz.com>
2017-08-03 18:57:57 +0800

    add job mq

--- edas-test-services/src/main/java/com/test/service/DemoProvider.java
+++ edas-test-services/src/main/java/com/test/service/DemoProvider.java
@@ -1,5 +1,6 @@
 package com.test.service;
 
+import com.test.mq.MQSenderManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,10 @@ public  class DemoProvider implements DemoApi {
 	@Autowired
 	private UserDao userDao;
 
+	@Autowired
+	MQSenderManager mqSenderManager;
+
+	@Override
 	public User selectUserById(Integer userId) {
 		logger.info("selectUserById, userId:{}", userId);
 		try {
@@ -24,9 +29,11 @@ public  class DemoProvider implements DemoApi {
 		} catch (Exception e) {
 			logger.error("selectUserById error", e);
 		}
+		mqSenderManager.testEDAS("edas selectUserById");
 		return userDao.selectUserById(userId);
 	}
-	
+
+	@Override
     public String getService() {
 		logger.info("getService, getService");
         return "hello,tiger.wang";
